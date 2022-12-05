9cf477aa86a9b1e44c4d7bde1e2685b05752bd3e
Ikaros <admin@yeekaros.net>
2022-12-06 02:50:09 +0800

    Refactor

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using url_shortener;
 using url_shortener.Models;
 using url_shortener.Repositories;
+using url_shortener.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,60 +31,8 @@ using (var conn = await databaseManager.GetDbConnection())
     await urlMappingRepository.initDb(conn);
 }
 
-
-app.MapPost("/", async (URLMappingInput body) =>
-{
-    using var conn = await databaseManager.GetDbConnection();
-    using (var transaction = await conn.BeginTransactionAsync())
-    {
-        try
-        {
-            var result = await urlMappingRepository.Create(body, conn: conn, transaction: transaction);
-            await transaction.CommitAsync();
-
-            return Results.Created($"/{result.code}", result);
-        }
-        catch (Exception e)
-        {
-            transaction.Rollback();
-            Console.WriteLine(e.ToString());
-            return Results.Problem(e.ToString());
-        }
-    }
-})
-.WithName("Create");
-
-app.MapGet("/", (context) =>
-{
-    context.Response.Headers.Add("Content-Type", "text/html");
-    return context.Response.SendFileAsync(Path.Combine("Pages", "index.html"));
-})
-.WithName("IndexPage");
-
-app.MapGet("/admin/list", (context) =>
-{
-    context.Response.Headers.Add("Content-Type", "text/html");
-    return context.Response.SendFileAsync(Path.Combine("Pages", "list.html"));
-})
-.WithName("AdminListPage");
-
-app.MapGet("/list", async () =>
-{
-    using var conn = await databaseManager.GetDbConnection();
-    var result = await urlMappingRepository.List(conn, getAccessLog: true);
-    return JsonSerializer.Serialize(new { data = result });
-}).WithName("List");
-
-app.MapGet("/{code}", async (string code) =>
-{
-    using var conn = await databaseManager.GetDbConnection();
-    var matched = await urlMappingRepository.Get(code, conn);
-    if (matched is null) return Results.NotFound(string.Format("Code {0} has no matched URL.", code));
-
-    await urlAccessLogRepository.Create(new URLAccessLog(code: code), conn);
-
-    return Results.Redirect(matched.url);
-}).WithName("RedirectWithCode");
+var apiGroup = app.MapGroup("/");
+(new URLMappingRoute(databaseManager, urlMappingRepository, urlAccessLogRepository)).Mount(apiGroup);
 
 app.Run();
 
---
+++ Source/Routes/URLMappingRoute.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.Json;
+using url_shortener.Models;
+using url_shortener.Repositories;
+
+namespace url_shortener.Routes
+{
+    public class URLMappingRoute
+    {
+        private DatabaseManager databaseManager;
+        private URLMappingRepository urlMappingRepository;
+        private URLAccessLogRepository urlAccessLogRepository;
+
+        public URLMappingRoute(DatabaseManager databaseManager, URLMappingRepository urlMappingRepository, URLAccessLogRepository urlAccessLogRepository) {
+            this.databaseManager = databaseManager;
+            this.urlMappingRepository = urlMappingRepository;
+            this.urlAccessLogRepository = urlAccessLogRepository;
+        }    
+
+        public RouteGroupBuilder Mount(RouteGroupBuilder group)
+        {
+            group.MapGet("/list", List).WithName(nameof(List));
+            group.MapPost("/", Create).WithName(nameof(Create));
+            group.MapGet("/{code}", RedirectWithCode).WithName(nameof(RedirectWithCode));
+            group.MapGet("/", IndexPage).WithName(nameof(IndexPage));
+            group.MapGet("/admin/list", AdminListPage).WithName(nameof(AdminListPage));
+            return group;
+        }
+
+        public async Task<string> List()
+        {
+            using var conn = await databaseManager.GetDbConnection();
+            var result = await urlMappingRepository.List(conn, getAccessLog: true);
+            return JsonSerializer.Serialize(new { data = result });
+        }
+
+        public async Task<IResult> Create(URLMappingInput body)
+        {
+            using var conn = await databaseManager.GetDbConnection();
+            using (var transaction = await conn.BeginTransactionAsync())
+            {
+                try
+                {
+                    var result = await urlMappingRepository.Create(body, conn: conn, transaction: transaction);
+                    await transaction.CommitAsync();
+
+                    return Results.Created($"/{result.code}", result);
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine(e.ToString());
+                    return Results.Problem(e.ToString());
+                }
+            }
+        }
+
+        public async Task<IResult> RedirectWithCode(string code)
+        {
+            using var conn = await databaseManager.GetDbConnection();
+            var matched = await urlMappingRepository.Get(code, conn);
+            if (matched is null) return Results.NotFound(string.Format("Code {0} has no matched URL.", code));
+
+            await urlAccessLogRepository.Create(new URLAccessLog(code: code), conn);
+
+            return Results.Redirect(matched.url);
+        }
+
+        public async Task<IResult> IndexPage()
+        {
+            return Results.File(Path.Combine(Environment.CurrentDirectory, "Pages", "index.html"), "text/html");
+        }
+
+        public async Task<IResult> AdminListPage()
+        {
+            return Results.File(Path.Combine(Environment.CurrentDirectory, "Pages", "list.html"), "text/html");
+        }
+
+
+    }
+}
